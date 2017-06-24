00fac432721de554b1d9ad5e7299939b565c4c8e
Jay Swan <jayswan@github.com>
2017-06-24 12:02:15 -0500

    remove extra comments

--- bitcoinmapping.py
+++ bitcoinmapping.py
@@ -10,21 +10,17 @@ seed_address = sys.argv[1]
 initialreq = "https://blockchain.info/rawaddr/{}".format(seed_address)
 
 firstjson = (requests.get(initialreq)).json()
-#graphvizlines = set()
 
 addresslist = set([seed_address])
 usedaddresslist = [seed_address]
 
 for transaction in firstjson["txs"]:
     
-    #print("\n{}".format(transaction["hash"]))
-
     payerlist = [item['prev_out']['addr'] for item in transaction['inputs']]
     recipientlist = [target['addr'] for target in transaction['out']]
 
     for payer in payerlist:
         for recipient in recipientlist:
-            #graphvizlines.add('"{}" -> "{}";'.format(payer,recipient))
             G.node(payer)
             G.node(recipient)
             G.edge(payer,recipient)
