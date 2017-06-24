0eb2c5197e4ff32c912cf2a5d5f2769a0022c3de
Jay Swan <jayswan@github.com>
2017-06-24 11:09:50 -0500

    further refactor

--- bitcoinmapping.py
+++ bitcoinmapping.py
@@ -7,41 +7,21 @@ seed_address = sys.argv[1]
 initialreq = "https://blockchain.info/rawaddr/{}".format(seed_address)
 
 firstjson = (requests.get(initialreq)).json()
-graphvizlines = []
+graphvizlines = set()
 
 addresslist = set([seed_address])
 usedaddresslist = [seed_address]
 
-for i in range(1,6):
-    if z is 1:
-        firstjson = (requests.get(initialreq)).json()
+for transaction in firstjson["txs"]:
     
-    for transaction in firstjson["txs"]:
-        payerlist = []
-        recipientlist = []
-        
-        print("\n{}".format(transaction["hash"]))
+    print("\n{}".format(transaction["hash"]))
 
-        for item in transaction["inputs"]:
-            payerlist.append(item["prev_out"]["addr"])
-            addresslist.add(item['prev_out']['addr'])
-            #if item["prev_out"]["addr"] not in addresslist:
-            #    addresslist.append(item["prev_out"]["addr"])
+    payerlist = [item['prev_out']['addr'] for item in transaction['inputs']]
+    recipientlist = [target['addr'] for target in transaction['out']]
 
-        for target in transaction["out"]:
-            recipientlist.append(target["addr"])
-            addresslist.add(target['addr'])
-            #if target["addr"] not in addresslist:
-            #    addresslist.append(target["addr"])
-
-        for payer in payerlist:
-            for recipient in recipientlist:
-                a = '"{}" -> "{};"'.format(payer,recipient)
-                if a not in graphvizlines:
-                    graphvizlines.append(a)
-    #i = i + 1    
-    z = 1
-        
+    for payer in payerlist:
+        for recipient in recipientlist:
+            graphvizlines.add('"{}" -> "{};"'.format(payer,recipient))
 
 for t in graphvizlines:
     print(t)
